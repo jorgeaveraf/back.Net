f25c3150b3a0c30738c0843a8312a7f919a02e33
jorgeaveraf <jorgeaveraf@gmail.com>
2024-10-27 12:35:35 -0600

    addition of CORS policy

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Agrega el HttpClient al contenedor de dependencias
+// Configurar CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFront", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500") // Cambia esto por el origen de tu frontend
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// Agrega el HttpClient y los controladores
 builder.Services.AddHttpClient();
-
-// Agrega los controladores al contenedor de dependencias
 builder.Services.AddControllers();
-
-// Configuración de Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Habilitar Swagger solo en desarrollo (opcional)
+// Usa CORS con la política definida
+app.UseCors("PermitirFront");
+
+// Swagger solo en desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
